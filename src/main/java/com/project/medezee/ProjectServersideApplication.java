65e0d0bfe32be7ec13928b11594baa335088cbd4
shubham <shubhamjadhav4899@gmail.com>
2024-01-15 00:07:44 +0530

    initial commit

---
+++ src/main/java/com/project/medezee/ProjectServersideApplication.java
@@ -0,0 +1,13 @@
+package com.project.medezee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectServersideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectServersideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/medezee/ProjectServersideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.medezee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectServersideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
